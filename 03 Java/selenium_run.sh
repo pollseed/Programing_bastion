4e40831fce04185192e3b7285fd5adaf9c9c72b9
pollseed <cb.so.devope+0001@gmail.com>
2014-09-03 00:25:37 +0900

    selenium, hubotのbatファイル

---
+++ 03 Java/selenium_run.sh
@@ -0,0 +1,3 @@
+# linux
+cd ~/develop
+java -jar selenium-server-standalone-2.42.2.jar -Dwebdriver.chrome.driver=~/chromedriver_???/chromedriver.exe
---
+++ shell/hubot_run.bat
@@ -0,0 +1,3 @@
+cd C:\develop\hubot\hubot\node_modules\hubot\node_modules\coffee-script\bin 
+node coffee C:\develop\hubot\hubot\node_modules\hubot\bin/hubot
+
