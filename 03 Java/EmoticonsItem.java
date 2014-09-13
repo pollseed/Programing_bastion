3b15bb2bcbe261472f2665c9b2a98a048bcbe088
shn <shn@soacmini.local>
2014-09-13 11:16:19 +0900

    顔文字のセットクラス追加→未完成

---
+++ 03 Java/EmoticonsItem.java
@@ -0,0 +1,9 @@
+
+
+/**
+* 特殊な顔文字のデータ・セットです（未完成）
+**/
+public class EmoticonsItem {
+  public final String EMOTICONS_BASE = "[ʕ·͡ˑ·ཻʔ|ʕ•̫͡•ིʔྀ|ʕु•̫͡•ʔु|ʕु-̫͡-ʔु”|ʕ•͡ɛ•͡ʼʼʔ|ʕ•͡-•ʔ|ʕ·͡ˑ·ཻʔ|ฅʕ•̫͡•ʔฅ|ʕ•͡౪•ʔ|ʕ•͡•ʔ|]";
+  public final String EMOTICONS_ITEM = "[✧|☂|♡*:.|♬|༄|]";
+}
